793b2fa642c8ab9af53816e3c00a803333a99aa8
NgDMau <maunguyen.dinh@gmail.com>
2023-11-18 17:33:23 +0900

    Fixed mfcc syntax error

--- paths.py
+++ paths.py
@@ -1,2 +1,2 @@
-buckeye_datadir = "/home/kamperh/backup/endgame/datasets/buckeye/"
+buckeye_datadir = "/home/ldap-users/Share/Corpora/BuckeyeSpeechCorpus_v2/"
 xitsonga_datadir = "/home/kamperh/backup/endgame/datasets/zerospeech2015/xitsonga_wavs/"
